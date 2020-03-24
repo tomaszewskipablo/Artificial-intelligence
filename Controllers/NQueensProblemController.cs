40dae484a494fbfcfc0046afbbe4c1f6c53b6b02
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-03-24 21:28:26 +0100

    Display chessboard

---
+++ Controllers/NQueensProblemController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArtificialIntelligence.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ArtificialIntelligence.Controllers
+{
+    public class NQueensProblemController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            Chessboard chessboard = new Chessboard(4);
+            return View(chessboard);
+            //return View();
+        }
+        public IActionResult displayChessboard()
+        {
+            Chessboard chessboard = new Chessboard(4);
+            return View(chessboard);
+        }
+    }
+}
---
+++ Models/Chessboard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtificialIntelligence.Models
+{
+    public class Chessboard
+    {
+        public int size=3;
+
+        public Chessboard(int size)
+        {
+            this.size = size;
+        }
+    }
+}
