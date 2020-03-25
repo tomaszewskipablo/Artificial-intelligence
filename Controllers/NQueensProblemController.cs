16d6294f5fa5fec2e574244d253c446aaa15c676
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-03-25 21:14:49 +0100

    Change chessboard size (POST method)

--- Controllers/NQueensProblemController.cs
+++ Controllers/NQueensProblemController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ArtificialIntelligence.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -18,6 +19,16 @@ namespace ArtificialIntelligence.Controllers
             return View(chessboard);
             //return View();
         }
+
+        [HttpPost]
+        public IActionResult NewSize(IFormCollection formCollection)
+        {            
+                int size = int.Parse(formCollection["size"]);
+
+                Chessboard chessboard = new Chessboard(size);
+                return View("Index", chessboard);           
+        }
+
         public IActionResult displayChessboard()
         {
             Chessboard chessboard = new Chessboard(9);
