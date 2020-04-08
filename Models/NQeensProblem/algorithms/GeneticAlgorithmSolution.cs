dff6ba262d5a6110554122b219e321ed0d9ac121
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-04-08 18:22:10 +0200

    implement mutation

--- Models/NQeensProblem/algorithms/GeneticAlgorithmSolution.cs
+++ Models/NQeensProblem/algorithms/GeneticAlgorithmSolution.cs
@@ -9,57 +9,89 @@ namespace ArtificialIntelligence.Models
 {
     public class GeneticAlgorithmSolution : ISolution
     {
-        public int heuristicSum=0;
+        public int heuristicSum = 0;
+        public int GenerationNumber;
         public double sum = 0;
         GeneticAlgorithmParameters parameters;
 
         public Chessboard solve(Chessboard board, IParams iparams)
         {
             parameters = (GeneticAlgorithmParameters)iparams;
-            board.steps = 0;
-            
-          
+            GenerationNumber = 0;
+
+
             List<Chessboard> Generation = new List<Chessboard>();
             List<Chessboard> NewGeneration = new List<Chessboard>();
 
             GenerateGenereation(Generation, parameters.sizeOfASingleGeneration, board.size);
-            
-            Sort(Generation);
 
-
-            //check if solved
-            if (Generation[0].finalHeuristic == 0)
+            while (GenerationNumber < parameters.numberOfGenerations)
             {
-                board.board = Generation[0].board;
-                board.isSolved = true;
-                board.finalHeuristic = board.Heuristic();
-                return board;
+                for (int i = 0; i < parameters.sizeOfASingleGeneration; i++)
+                {
+                    Generation[i].finalHeuristic = Generation[i].Heuristic();
+                }
+                Sort(Generation);
+
+
+                //check if solved
+                if (Generation[0].finalHeuristic == 0)
+                {
+                    board.board = Generation[0].board;
+                    board.steps = GenerationNumber;
+                    board.isSolved = true;
+                    board.finalHeuristic = board.Heuristic();
+                    return board;
+                }
+
+                // elitizim
+                for (int i = 0; i < parameters.percentOfElitism; i++)
+                {
+                    NewGeneration.Add(Generation[i]);
+                }
+
+
+                CountHeuristicOfWholePopulation(Generation);
+
+                while (NewGeneration.Count != Generation.Count)
+                {
+                    Chessboard child = new Chessboard(board.size);
+
+                    //selection
+                    Chessboard parentA = SelectParent(Generation);
+                    Chessboard parentB = SelectParent(Generation);
+
+                    //crossover
+                    child = Crossover(parentA, parentB);
+
+                    // mutate
+                    Mutate(child);
+
+                    NewGeneration.Add(child);
+                }
+
+                GenerationNumber++;
+                NewGenerationBecomesParentsGeneration(Generation, NewGeneration);
             }
-
-            //NewGeneration.Add()
-
-            //
-            CountHeuristicOfWholePopulation(Generation);
-
-            while (NewGeneration.Count != Generation.Count)
-            {
-                Chessboard child = new Chessboard(board.size);
-                
-                //selection
-                Chessboard parentA = SelectParent(Generation);
-                Chessboard parentB = SelectParent(Generation);
-
-                //crossover
-                child = Crossover(parentA, parentB);
-
-                // mutate
-
-                NewGeneration.Add(child);
-
-
-            }
-
+            board.steps = parameters.numberOfGenerations;
             return Generation[0];
+        }
+        private void Mutate(Chessboard child)
+        {
+            Random rnd = new Random();
+            if(rnd.Next(0, 100) < parameters.mutationProbability)
+            {
+                child.MoveRandomlyOneQueen();
+            }            
+        }
+        private void NewGenerationBecomesParentsGeneration(List<Chessboard> Generation, List<Chessboard> NewGeneration)
+        {
+            Generation.Clear();
+            for (int i = 0; i < parameters.sizeOfASingleGeneration; i++)
+            {
+                Generation.Add(NewGeneration[i]);
+            }
+            NewGeneration.Clear();
         }
         private void GenerateGenereation(List<Chessboard> Generation, int sizeOfGeneration, int sizeOfBoard)
         {
@@ -68,34 +100,36 @@ namespace ArtificialIntelligence.Models
                 Generation.Add(new Chessboard(sizeOfBoard));
             }
         }
-        private Chessboard SelectParent(List<Chessboard> Generation) 
+        private Chessboard SelectParent(List<Chessboard> Generation)
         {
             Random rnd = new Random();
             int random = rnd.Next(0, (int)sum);
 
-            double temp=0;
+            double temp = 0;
 
-            for(int i=0;i< parameters.sizeOfASingleGeneration; i++)
+            for (int i = 0; i < parameters.sizeOfASingleGeneration; i++)
             {
-                temp += heuristicSum/Generation[i].finalHeuristic;
+                temp += heuristicSum / Generation[i].finalHeuristic;
                 if (random < temp)
                 {
                     return Generation[i];
                 }
-                
+
             }
-            return Generation[0];      
+            return Generation[0];
         }
         private Chessboard Crossover(Chessboard parentA, Chessboard parentB)
         {
             Chessboard child = new Chessboard(parentA.size);
 
-            int split = parentA.size / 2;
+            Random rnd = new Random();
+            int split = rnd.Next(0, parentA.size);
+
             for (int i = 0; i < split; i++)
             {
                 child.board[i] = parentA.board[i];
-             }
-            for (int i = split /2; i < parentA.size; i++)
+            }
+            for (int i = split; i < parentA.size; i++)
             {
                 child.board[i] = parentB.board[i];
             }
@@ -121,9 +155,9 @@ namespace ArtificialIntelligence.Models
         private void CountHeuristicOfWholePopulation(List<Chessboard> Generation)
         {
             heuristicSum = 0;
-            for (int i=0;i<parameters.sizeOfASingleGeneration; i++)
+            for (int i = 0; i < parameters.sizeOfASingleGeneration; i++)
             {
-                heuristicSum += Generation[i].finalHeuristic;                
+                heuristicSum += Generation[i].finalHeuristic;
             }
             for (int i = 0; i < parameters.sizeOfASingleGeneration; i++)
             {
