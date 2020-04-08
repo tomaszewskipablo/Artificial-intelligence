9723be49a309713703ee7433aa498ff677e5d259
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-04-08 14:05:02 +0200

    Sort generation function

--- Models/NQeensProblem/algorithms/GeneticAlgorithmSolution.cs
+++ Models/NQeensProblem/algorithms/GeneticAlgorithmSolution.cs
@@ -1,4 +1,5 @@
 ﻿using ArtificialIntelligence.Models.NQeensProblem;
+using ArtificialIntelligence.Models.NQeensProblem.Parameters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,46 @@ namespace ArtificialIntelligence.Models
     {
         public Chessboard solve(Chessboard board, IParams iparams)
         {
-            Chessboard chessboard = board;
+            GeneticAlgorithmParameters parameters = (GeneticAlgorithmParameters)iparams;
+            board.steps = 0;
+            
+            //Generate X random states and save them into states list.
+            List<Chessboard> Generation = new List<Chessboard>();
+
+            generateGenereation(Generation, parameters.numberOfGenerations, board.size);
+
+            Sort(Generation);
+
+            Chessboard f = new Chessboard(4);
 
 
-            return chessboard;
+
+
+            return f;
+        }
+        private void generateGenereation(List<Chessboard> Generation, int sizeOfGeneration, int sizeOfBoard)
+        {
+            for (int i = 0; i < sizeOfGeneration; i++)
+            {
+                Generation.Add(new Chessboard(sizeOfBoard));
+            }
+        }
+        private void Sort(List<Chessboard> Generation)
+        {
+            // BUBLE SORT
+            Chessboard temp;
+            for (int j = 0; j <= Generation.Count - 2; j++)
+            {
+                for (int i = 0; i <= Generation.Count - 2; i++)
+                {
+                    if (Generation[i].finalHeuristic > Generation[i + 1].finalHeuristic)
+                    {
+                        temp = Generation[i + 1];
+                        Generation[i + 1] = Generation[i];
+                        Generation[i] = temp;
+                    }
+                }
+            }
         }
 
     }
