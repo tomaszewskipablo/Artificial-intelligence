2b155efa644bf26d65bd9b731a6f60468e9069da
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-04-08 16:15:44 +0200

    implement selection function

--- Models/NQeensProblem/algorithms/GeneticAlgorithmSolution.cs
+++ Models/NQeensProblem/algorithms/GeneticAlgorithmSolution.cs
@@ -9,36 +9,82 @@ namespace ArtificialIntelligence.Models
 {
     public class GeneticAlgorithmSolution : ISolution
     {
+        public int heuristicSum=0;
+        public double sum = 0;
+        GeneticAlgorithmParameters parameters;
+
         public Chessboard solve(Chessboard board, IParams iparams)
         {
-            GeneticAlgorithmParameters parameters = (GeneticAlgorithmParameters)iparams;
+            parameters = (GeneticAlgorithmParameters)iparams;
             board.steps = 0;
             
           
             List<Chessboard> Generation = new List<Chessboard>();
+            List<Chessboard> NewGeneration = new List<Chessboard>();
 
-            generateGenereation(Generation, parameters.numberOfGenerations, board.size);
-
+            GenerateGenereation(Generation, parameters.sizeOfASingleGeneration, board.size);
+            
             Sort(Generation);
 
-            
 
+            //check if solved
             if (Generation[0].finalHeuristic == 0)
             {
                 board.board = Generation[0].board;
                 board.isSolved = true;
-                board.finalHeuristic = board.Heuristic();               
+                board.finalHeuristic = board.Heuristic();
+                return board;
+            }
+
+            //NewGeneration.Add()
+
+            //
+            CountHeuristicOfWholePopulation(Generation);
+
+            while (NewGeneration.Count != Generation.Count)
+            {
+                Chessboard child = new Chessboard(board.size);
+                
+                //selection
+                Chessboard parentA = SelectParent(Generation);
+                Chessboard paerntB = SelectParent(Generation);
+
+                //crossover
+
+
+                // mutate
+
+                NewGeneration.Add(child);
+
+
             }
 
             return Generation[0];
         }
-        private void generateGenereation(List<Chessboard> Generation, int sizeOfGeneration, int sizeOfBoard)
+        private void GenerateGenereation(List<Chessboard> Generation, int sizeOfGeneration, int sizeOfBoard)
         {
             for (int i = 0; i < sizeOfGeneration; i++)
             {
                 Generation.Add(new Chessboard(sizeOfBoard));
             }
         }
+        private Chessboard SelectParent(List<Chessboard> Generation) 
+        {
+            Random rnd = new Random();
+            int random = rnd.Next(0, (int)sum);
+
+
+            for(int i=0;i< parameters.sizeOfASingleGeneration; i++)
+            {
+                if(random > heuristicSum)
+                {
+                    return Generation[i];
+                }
+                heuristicSum += Generation[i].finalHeuristic/ heuristicSum;
+            }
+            return Generation[0];      
+        }
+
         private void Sort(List<Chessboard> Generation)
         {
             // BUBLE SORT
@@ -54,6 +100,18 @@ namespace ArtificialIntelligence.Models
                         Generation[i] = temp;
                     }
                 }
+            }
+        }
+        private void CountHeuristicOfWholePopulation(List<Chessboard> Generation)
+        {
+            heuristicSum = 0;
+            for (int i=0;i<parameters.sizeOfASingleGeneration; i++)
+            {
+                heuristicSum += Generation[i].finalHeuristic;                
+            }
+            for (int i = 0; i < parameters.sizeOfASingleGeneration; i++)
+            {
+                sum += (double)heuristicSum / Generation[i].finalHeuristic;
             }
         }
 
