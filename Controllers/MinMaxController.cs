6f5c97f1fc938e6bd334b890c13d1e5482ac992c
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-04-16 13:43:13 +0200

    Controller action for Game  core

--- Controllers/MinMaxController.cs
+++ Controllers/MinMaxController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ArtificialIntelligence.Models.MinMax;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ArtificialIntelligence.Controllers
@@ -10,7 +12,23 @@ namespace ArtificialIntelligence.Controllers
     {
         public IActionResult Index()
         {
+
             return View("MinMax");
+        }
+
+        [HttpPost]
+        public IActionResult Game(IFormCollection formCollection)
+        {
+            GameState gameState = GameState.Instance;
+            //var x = formCollection["x"];
+            //var y = formCollection["Y"];
+
+            // Model.MakeMove(x,y); check if space is free, if free save, if not dont save
+            // Model.CheckWin()
+            // Model.AIMove()
+
+            return View("MinMax");
+
         }
     }
 }
