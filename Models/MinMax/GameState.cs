bf5ab7302669e595f15585d5ec4abf09ac57a6c7
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-04-16 13:42:25 +0200

    implement singlteon for GamesState class

--- Models/MinMax/GameState.cs
+++ Models/MinMax/GameState.cs
@@ -7,5 +7,22 @@ namespace ArtificialIntelligence.Models.MinMax
 {
     public class GameState
     {
+        private static GameState instance = null;
+        private GameState() { }
+
+        public static GameState Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new GameState();
+                }
+                return instance;
+            }
+        }
+
+        private Player[] player;
+        // enums
     }
 }
