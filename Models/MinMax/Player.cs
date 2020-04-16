fa508c898e67ca2979856194de8a8eaf654effc2
Paweł Tomaszewski <32485994+tomaszewskipablo@users.noreply.github.com>
2020-04-16 14:22:21 +0200

    create form behing the board, every field is a button with numeric (0-8) id

--- Models/MinMax/Player.cs
+++ Models/MinMax/Player.cs
@@ -7,5 +7,24 @@ namespace ArtificialIntelligence.Models.MinMax
 {
     public class Player
     {
+        private int wins;
+        private bool isCross;
+
+        public Player(bool isCross)
+        {
+            this.isCross = isCross;
+        }
+        public void AddWin()
+        {
+            wins++;
+        }
+        public int GetWin()
+        {
+            return wins;
+        }
+        public bool GetIsCross()
+        {
+            return isCross;
+        }
     }
 }
